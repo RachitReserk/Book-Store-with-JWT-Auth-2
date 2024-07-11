5aa425bf013bea741dc57454d51216649ab06545
RachitReserk <trachit752@gmail.com>
2024-07-11 09:18:43 +0530

    added add to cart api

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ dotenv.config()
 import userRouter from './controllers/userManagement.js'
 import bookRouter from './controllers/bookManagement.js'
 import favRouter from './controllers/favourite.js'
+import cartRouter from './controllers/carts.js'
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log('Connected to MongoDB')
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 app.use("/api",userRouter)
 app.use('/api',bookRouter)
 app.use('/api',favRouter)
+app.use("/api",cartRouter)
 
 app.listen(process.env.PORT,() => {
     console.log(`Server started at port ${process.env.PORT}`)
---
+++ server/controllers/carts.js
@@ -0,0 +1,64 @@
+import express from 'express'
+const cartRouter = express.Router()
+import authenticateToken from './userAuth.js'
+import User from '../models/user.js'
+
+cartRouter.put("/add-to-cart",authenticateToken,async (req,res) => {
+    try {
+         const {bookid,id} = req.headers;
+         const userData = await User.findById(id);
+         const isBookFavourite = userData.cart.includes(bookid)
+         if(isBookFavourite){
+            return res.json(
+                {
+                    status:"Success",
+                    message:"Book is already in cart"
+                }
+            )
+         }
+         await User.findByIdAndUpdate(id,{
+            $push:{ cart:bookid}
+         })
+
+         return res.json({
+            status:"Success",
+            "message":"Book added to cart"
+         })
+    } catch (error) {
+        res.status(500).json({message:"Internal server error"})
+    }
+})
+
+cartRouter.put("remove-from-cart/:bookid",authenticateToken,async (req,res) => {
+try {
+    const {bookid} = req.params
+    const {id} = req.headers
+    await User.findByIdAndUpdate(id,{
+        $pull:{cart:bookid}
+    })
+
+    return res.json({
+        status:"Success",
+        message:"Removed from cart"
+     })
+} catch (error) {
+    console.log(error)
+    res.status(500).json({message:"Internal Server Error"})
+}    
+})
+
+cartRouter.get("/get-cart",authenticateToken,async(req,res) => {
+    try {
+        const {id} = req.headers
+        const userData = await User.findById(id)
+        const cart = userData.cart.reverse()
+        console.log(cart)
+        return res.json({
+            status:"success",
+            data:cart
+        })
+        } catch (error) {
+        res.status(500).json({message:"Internal Server error"})
+    }
+})
+export default cartRouter
