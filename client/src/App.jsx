27c896b5e644b7d6caaf17662dd2c6263495daca
RachitReserk <trachit752@gmail.com>
2024-07-16 21:26:14 +0530

    made the front-end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Home from './pages/Home.jsx'
+import Navbar from './components/NavBar/NavBar.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import {BrowserRouter as Router , Routes , Route} from "react-router-dom"
+import Store from './pages/Store.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+import Profile from './pages/Profile.jsx'
+import Cart from './pages/Cart.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="cursor-default realtive overflow-x-hidden bg-white2 text-dark">
+      <Router>
+      <Navbar/>
+        <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+          <Route path='/store' element={<Store/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/signin' element={<SignIn/>}></Route>
+          <Route path='/signup' element={<SignUp/>}></Route>
+          <Route path='/cart' element={<Cart/>}></Route>
+          <Route path='/profile' element={<Profile/>}></Route>
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   )
 }
 
---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,254 @@
+import React from "react"
+import Logo from '/src/assets/bookstore.png'
+import { FaUserLarge } from "react-icons/fa6";
+import { FaCartShopping } from "react-icons/fa6";
+import {motion } from 'framer-motion';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Hamburger from 'hamburger-react'
+
+const NavMenu = [
+    {
+        id:1,
+        title: "Home",
+        path:"/",
+        delay:0.3,
+    },{
+        id:2,
+        title: "Store",
+        path:"/store",
+        delay:0.3,
+    },{
+        id:3,
+        title: "About Us",
+        path:"/about",
+        delay:0.3,
+    },{
+        id:4,
+        title: "Contact Us",
+        path:"/contact",
+        delay:0.3,
+    },
+]
+
+const Slidedown = (delay) => {
+    return{
+        initial:{
+            y:"-100%",
+            opacity: 0,
+        },
+        animate :{
+            y:0,
+            opacity: 1,
+            transition:{
+                duration: 0.6,
+                delay: delay,
+            },
+        },
+    };
+};
+
+const Navbar = () => {
+  const [user , setUser] = useState(null)
+  const [isOpen, setOpen] = useState(false)
+
+  if(user != null)
+  return <nav>
+    <div className="z-[50] container mx-auto flex justify-between items-center font-league">
+        {/* logo section */}
+        <motion.img
+        initial = {{opacity : 0}}
+        animate = {{opacity : 1}}
+        transition={{duration : 0.8,delay : 0.5}}
+        src={Logo}
+        alt=""
+        className="w-24 block"
+        />
+        {/* menu section */}
+         <div className= "hidden md:block">
+           <ul className= "flex gap-12">
+            {
+              NavMenu.map((menu) => {
+                return(
+                    <motion.li
+                       variants={Slidedown(menu.delay)}
+                       initial = "initial"
+                       animate = "animate"
+                       
+                       key={menu.id}
+                       className="nav-menu"
+                       date-delay = {menu.delay}
+                       >
+                        <Link to={menu.path} className="inline-block px-2 py-2 text-2xl max-w-xs transition duration-300 ease-in-out hover:scale-110"
+                        >
+                            {menu.title}
+                            </Link>
+                       </motion.li>
+                );
+              }
+            )
+            }
+            </ul> 
+         </div>
+        {/* button section */}
+        <div className="flex gap-6 hidden md:flex">
+        <Link to='/profile'>
+        <motion.div variants={Slidedown(1)}
+        initial = "initial" animate = "animate">
+            <button className= "max-w-xs transition duration-300 ease-in-out hover:scale-110 h-[50px] w-[50px] grid place-items-center rounded-full text-white bg-black">
+                <FaUserLarge className="h-[18px] w-[18px]" /></button>
+        </motion.div>
+        </Link>
+        <Link to='/cart'>
+        <motion.div variants={Slidedown(1)}
+        initial = "initial" animate = "animate">
+            <button className= "max-w-xs transition duration-300 ease-in-out hover:scale-110 h-[50px] w-[50px] grid place-items-center rounded-full text-white bg-black">
+                <FaCartShopping className="h-[18px] w-[18px]" /></button>
+        </motion.div>
+        </Link>
+        </div>
+        <div className="md:hidden w-[80px] h-[80px] rounded-3xl mt-5 mr-3">
+                <div className="flex mt-4">
+                <Hamburger size={40} toggled={isOpen} toggle={setOpen}></Hamburger>
+                </div>
+            </div>
+    </div>
+    {isOpen ? 
+      <div className="h-[50] rounded font-league z-[40] flex flex-col top-0 left-0 bg-orange-200 items-center gap-12 justify-between">
+      {
+              NavMenu.map((menu) => {
+                return(
+                    <motion.li
+                       variants={Slidedown(menu.delay)}
+                       initial = "initial"
+                       animate = "animate"
+                       
+                       key={menu.id}
+                       className="nav-menu list-none border-b-2 mt-5 mb-5"
+                       date-delay = {menu.delay}
+                       >
+                        <Link to={menu.path} className="text-4xl max-w-xs transition duration-300 ease-in-out hover:scale-110"
+                        >
+                            {menu.title}
+                            </Link>
+                       </motion.li>
+                );
+              }
+            )
+            }
+            <div className="flex gap-6 mb-5">
+        <Link to='/profile'>
+        <motion.div variants={Slidedown(1)}
+        initial = "initial" animate = "animate">
+            <button className= "max-w-xs transition duration-300 ease-in-out hover:scale-110 h-[50px] w-[50px] grid place-items-center rounded-full text-white bg-black">
+                <FaUserLarge className="h-[18px] w-[18px]" /></button>
+        </motion.div>
+        </Link>
+        <Link to='/cart'>
+        <motion.div variants={Slidedown(1)}
+        initial = "initial" animate = "animate">
+            <button className= "max-w-xs transition duration-300 ease-in-out hover:scale-110 h-[50px] w-[50px] grid place-items-center rounded-full text-white bg-black">
+                <FaCartShopping className="h-[18px] w-[18px]" /></button>
+        </motion.div>
+        </Link>
+        </div>
+      </div> 
+      :
+       <div></div>}
+  </nav>;
+
+else if(user == null)
+    return <nav className="">
+      <div className="z-[50] container mx-auto flex justify-between items-center font-league">
+          {/* logo section */}
+          <motion.img
+          initial = {{opacity : 0}}
+          animate = {{opacity : 1}}
+          transition={{duration : 0.8,delay : 0.5}}
+          src={Logo}
+          alt=""
+          className="w-24 block"
+          />
+          {/* menu section */}
+           <div className= "hidden md:block">
+             <ul className= "flex gap-12">
+              {
+                NavMenu.map((menu) => {
+                  return(
+                      <motion.li
+                         variants={Slidedown(menu.delay)}
+                         initial = "initial"
+                         animate = "animate"
+                         
+                         key={menu.id}
+                         className="nav-menu"
+                         date-delay = {menu.delay}
+                         >
+                          <Link to={menu.path} className="inline-block px-2 py-2 text-2xl max-w-xs transition duration-300 ease-in-out hover:scale-110"
+                          >
+                              {menu.title}
+                              </Link>
+                         </motion.li>
+                  );
+                }
+              )
+              }
+              </ul>
+           </div>
+           <div className="hidden md:block">
+           <motion.li variants={Slidedown(1)}
+                         initial = "initial"
+                         animate = "animate"
+                         className="nav-menu flex gap-6"
+                         date-delay = {0.3}>
+                        <Link to="/signin" className="inline-block px-2 py-2 text-2xl border-2 max-w-xs transition duration-300 ease-in-out hover:scale-110 bg-blue-200 rounded">Sign-in</Link>
+                        <Link to="/signup" className="inline-block px-2 py-2 text-2xl border-2 max-w-xs transition duration-300 ease-in-out hover:scale-110 rounded">Sign-up</Link>
+                         </motion.li>
+            </div>
+            <div className="md:hidden w-[80px] h-[80px] rounded-3xl mt-5 mr-3">
+                <div className="flex mt-4">
+                <Hamburger size={40} toggled={isOpen} toggle={setOpen}></Hamburger>
+                </div>
+            </div>
+      </div>
+      {isOpen ? 
+      <div className="h-[50] rounded font-league z-[40] flex flex-col top-0 left-0 bg-orange-200 items-center gap-12 justify-between">
+      {
+              NavMenu.map((menu) => {
+                return(
+                    <motion.li
+                       variants={Slidedown(menu.delay)}
+                       initial = "initial"
+                       animate = "animate"
+                       
+                       key={menu.id}
+                       className="nav-menu list-none border-b-2 mt-5 mb-5"
+                       date-delay = {menu.delay}
+                       >
+                        <Link to={menu.path} className="text-4xl max-w-xs transition duration-300 ease-in-out hover:scale-110"
+                        >
+                            {menu.title}
+                            </Link>
+                       </motion.li>
+                );
+              }
+            )
+            }
+            <div className="mb-5">
+           <motion.li variants={Slidedown(1)}
+                         initial = "initial"
+                         animate = "animate"
+                         className="nav-menu flex gap-6"
+                         date-delay = {0.3}>
+                        <Link to="/signin" className="inline-block px-2 py-2 text-2xl border-2 max-w-xs transition duration-300 ease-in-out hover:scale-110 bg-blue-200 rounded">Sign-in</Link>
+                        <Link to="/signup" className="inline-block px-2 py-2 text-2xl border-2 max-w-xs transition duration-300 ease-in-out hover:scale-110 rounded">Sign-up</Link>
+                         </motion.li>
+            </div>
+      </div> 
+      :
+       <div></div>}
+    </nav>;
+
+};
+
+export default Navbar
