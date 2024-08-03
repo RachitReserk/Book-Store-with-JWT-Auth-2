03b30c22e87595460b8bba41522515a9b246792b
RachitReserk <trachit752@gmail.com>
2024-08-03 16:31:22 +0530

    major update v12

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,78 @@
 import React from 'react'
+import BannerPng from "../assets/about.png";
+import { motion } from "framer-motion";
+import { SlideUp } from "../components/Hero/Hero";
+import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
-    <div>
-      
+    <section className=' h-screen '>
+    <div className="space-y-6 continer py-24">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-14 place-items-center">
+        {/* Image section */}
+        <div className="relative h-[400px] w-[400px]">
+          <motion.img
+            initial={{
+              opacity: 0,
+              x: -100,
+              y: 100,
+            }}
+            whileInView={{
+              opacity: 1,
+              x: 0,
+              y: 0,
+            }}
+            whileHover={{
+              scale: 1.2,
+              x: 0,
+              y: 0,
+            }}
+            transition={{
+              duration: 0.5,
+              delay: 0.5,
+              scale: { duration: 0.5 },
+            }}
+            src={BannerPng}
+            alt=""
+            className="relative z-10 w-full lg:max-[350px] img-shadow"
+          />
+        </div>
+
+        {/* Text section */}
+        <div className="space-y-8 lg:max-w-[800px]">
+            <motion.h1 
+             variants={SlideUp(1)}
+             initial = "hidden"
+             whileInView="show"
+            className=" text-center text-5xl uppercase font-league">About Us</motion.h1>
+
+            <motion.h2 
+             variants={SlideUp(1.1)}
+             initial = "hidden"
+             whileInView="show"
+            className="text-3xl uppercase font-league">Welcome to Book Store</motion.h2>
+
+            <motion.p variants={SlideUp(1.2)}
+             initial = "hidden"
+             whileInView="show">At Book Store, we're dedicated to the magic of books and the joy of reading. Beyond just shelves filled with stories, we're a vibrant community hub where book lovers gather to explore, connect, and discover new adventures in every chapter.</motion.p>
+            
+            <motion.p variants={SlideUp(1.4)}
+             initial = "hidden"
+             whileInView="show">Step inside and explore our handpicked collection, carefully curated to inspire and delight. From timeless classics to thrilling new releases, each book is chosen with passion and care to ensure there's something for every reader's imagination. Founded with a commitment to spreading the love of literature, Book Store invites you to dive into the world of books and celebrate the power of storytelling.</motion.p>
+            <Link to='/store'>
+            <motion.button
+              variants={SlideUp(1.6)}
+              initial = "hidden"
+              whileInView="show"
+            className="btn-primary">
+                Explore
+            </motion.button>
+            </Link>
+         </div>
+
+      </div>
     </div>
+  </section>
   )
 }
 
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,10 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { motion } from 'framer-motion'
+import { SlideUp } from '../components/Hero/Hero'
+import {Link, useNavigate} from 'react-router-dom'
+import { toast } from 'react-toastify'
+
 
 const Contact = () => {
+  const [info,setInfo] = useState()
+  const [name,setName] = useState()
+  const [email,setEmail] = useState()
+  const [message,setMessage] = useState()
+  const navigate = useNavigate()
+
+  const submit = () => {
+    toast('Message sent !')
+    navigate('/')
+  }
+  
   return (
-    <div>
-      
+    <div className='bg-yellow-100 lg:h-screen h-auto py-8 px-12 flex items-center justify-center'>
+    <form onSubmit={submit} className='bg-yellow-200 rounded-lg px-8 py-5 w-full md:w-3/6 lg:w-2/6'>
+    <motion.h1
+           variants={SlideUp(0.1)}
+           initial = "hidden"
+           whileInView = "show" 
+           className="">
+    <p className='text-xl'>Contact Us</p>
+    <div className='mt-4'>
+     <div>
+       <label htmlFor='' className=''>
+         Email
+       </label>
+       <input 
+       type='email'
+       className='w-full mt-2 p-2 outline-none'
+       placeholder='email'
+       name='email'
+       required
+       value={email}
+       onChange={(e) => setEmail(e.value)}
+       ></input>
+     </div>
+     <div className='mt-4'>
+       <label htmlFor=''>
+       Name
+       </label>
+       <input 
+       type='text'
+       className='w-full mt-2 p-2 outline-none'
+       placeholder='name'
+       name='name'
+       value={name}
+       onChange={(e) => setName(e.value)}
+       > 
+       </input>
+     </div>
+     <div className='mt-4'>
+       <label htmlFor=''>
+       Message
+       </label>
+       <textarea 
+       type='text'
+       className='w-full h-[30vh] mt-2 p-2 outline-none'
+       placeholder='message'
+       name='message'
+       value={message}
+       onChange={(e) => setMessage(e.value)}
+       > 
+       </textarea>
+     </div>
+     <div className='mt-4'>
+       <button className='transition duration-300 ease-in-out hover:scale-110 w-full bg-yellow-300 font-semibold py-2 rounded' type="submit">SEND</button>
+     </div>
     </div>
+    </motion.h1>
+    </form>
+    </div>  
   )
 }
 
